d494c492d822b4e77575f5f8d99559ba4e8236b0
LeandroDeJesus-S <jstleandro@gmail.com>
2024-07-08 00:46:31 -0300

    feat: front de reservas finalizado

--- reservas/views.py
+++ reservas/views.py
@@ -3,29 +3,32 @@ from django.views import View
 from django.core.exceptions import ValidationError
 
 from . import validators
+from .models import Classe
 
 
 class Reserva(View):
     def get(self, *args, **kwargs):
-        return render(self.request, 'reserva.html')
+        context = {
+            'room_classes': Classe.objects.all(),
+        }
+        return render(self.request, 'reserva.html', context)
     
     def post(self, *args, **kwargs):
-        NAME = self.request.POST.get('nome')
-        CHECK_IN = self.request.POST.get('chegada')
-        CHECKOUT = self.request.POST.get('saida')
-        QTD_ADULTS = self.request.POST.get('qtdAdultos')
-        QTD_CHILDREN = self.request.POST.get('qtdCriacas')
-        ROOM_TYPE = self.request.POST.get('tipoQuarto')
+        NAME = self.request.POST.get('cliente-nome')
+        CHECK_IN = self.request.POST.get('checkin')
+        CHECKOUT = self.request.POST.get('checkout')
+        QTD_ADULTS = self.request.POST.get('adultos')
+        QTD_CHILDREN = self.request.POST.get('criacas')
         PHONE = self.request.POST.get('telefone')
         EMAIL = self.request.POST.get('email')
         
-        try:
-            validators.validate_fullname(NAME)
-            validators.validate_checkin_and_checkout(CHECK_IN, CHECKOUT)
-            validators.validate_qtd_adultos(QTD_ADULTS)
-        except ValidationError as msg_list:
-            print(msg_list)
+        # try:
+        #     validators.validate_fullname(NAME)
+        #     validators.validate_checkin_and_checkout(CHECK_IN, CHECKOUT)
+        #     validators.validate_qtd_adultos(QTD_ADULTS)
+        # except ValidationError as msg_list:
+        #     print(msg_list)
         
-        print(CHECK_IN, CHECKOUT)
+        # print(CHECK_IN, CHECKOUT)
         return HttpResponse(self.request.POST.items())
 
