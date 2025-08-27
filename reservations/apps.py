7dcb4d14d69733c60acf2c2190051671efc0dfae
LeandroDeJesus-S <jstleandro@gmail.com>
2025-08-27 01:10:10 -0300

    fix schedule creation for reservations when running migrate command

--- reservations/apps.py
+++ reservations/apps.py
@@ -6,11 +6,15 @@ class ReservationsConfig(AppConfig):
     name = 'reservations'
 
     def ready(self) -> None:
-        from django_q.models import Schedule
+        try:
+            from django.db.utils import OperationalError
+            from django_q.models import Schedule
 
-        if not Schedule.objects.filter(name='checar finalização das reservas').exists():
-            Schedule.objects.create(
-                func='reservations.tasks.check_reservation_dates',
-                schedule_type=Schedule.DAILY,
-                name='checar finalização das reservas',
-            )
+            if not Schedule.objects.filter(name='checar finalização das reservas').exists():
+                Schedule.objects.create(
+                    func='reservations.tasks.check_reservation_dates',
+                    schedule_type=Schedule.DAILY,
+                    name='checar finalização das reservas',
+                )
+        except OperationalError:
+            pass
